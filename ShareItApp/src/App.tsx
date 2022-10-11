f4a4f35719baca3fe090f2e541bc45cbe2db5297
Jakub Kliś <jakubklis2201@gmail.com>
2022-10-11 13:01:03 +0200

    Background

--- ShareItApp/src/App.tsx
+++ ShareItApp/src/App.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import {BrowserRouter, Route, Routes} from "react-router-dom"
 import Login from './Login';
 import WelcomePage from './WelcomePage';
+import './Component/css/main.css';
 
 function App() {
   return (
