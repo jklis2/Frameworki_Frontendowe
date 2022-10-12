cfaaef64c538a7053ca2884af403c26f8653a2b9
Matd127 <79983536+Matd127@users.noreply.github.com>
2022-10-12 14:38:45 +0200

    Downloading data from API

--- ShareItApp/src/WelcomePage.tsx
+++ ShareItApp/src/WelcomePage.tsx
@@ -1,10 +1,30 @@
-import React from "react";
+import React, { useEffect } from "react";
+import { useState } from "react";
+import User from "./Entities/Users";
 
 const WelcomePage = () => {
+
+    const [data, setData] = useState<Array<User>>([])
+    
+    const fetchData = async () => {
+        const request = await fetch('https://jsonplaceholder.typicode.com/users')
+        const response = await request.json()
+        setData(response)
+    }
+   
+useEffect(() => {
+    fetchData()
+}, [])   
+    
     return(
     <div> 
-        Welcome
+        {data.map((items) => {
+            return (
+                <div key={items.id}> {items.username}</div>
+            )
+        })}
     </div>
+
     )
 }
 
