622f401856f848f518e0c333b4bc9bf7a5bb3e6a
Jakub Kliś <jakubklis2201@gmail.com>
2022-10-12 18:08:31 +0200

    NavBar Fix

--- ShareItApp/src/Component/tsx/Header.tsx
+++ ShareItApp/src/Component/tsx/Header.tsx
@@ -7,11 +7,11 @@ import Navbar from 'react-bootstrap/Navbar';
 const Header = () => {
     return (<div>
       <Navbar className="navbar" bg="dark" variant="dark">
-        <Container>
+        <Container className="navbar-container">
           <Navbar.Brand href="#home">
             <img src={`${process.env.PUBLIC_URL}/img/Logo.png`} alt="Logo ShareItNow"/>
           </Navbar.Brand>
-          <Nav className="me-auto">
+          <Nav className="ms-auto">
             <Nav.Link href="#home">Home</Nav.Link>
             <Nav.Link href="#features">Features</Nav.Link>
             <Nav.Link href="#pricing">Pricing</Nav.Link>
