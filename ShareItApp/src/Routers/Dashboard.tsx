36154d46d49d3a95ad0bc301c876dcd58d5ea762
Jakub Kliś <jakubklis2201@gmail.com>
2022-10-19 17:40:41 +0200

    Dashboard left menu fix

--- ShareItApp/src/Routers/Dashboard.tsx
+++ ShareItApp/src/Routers/Dashboard.tsx
@@ -8,23 +8,37 @@ const Dashboard: FC = () => {
       <Header />
     <div className="container">
     <div className="dashboard-left-menu ">
-        <img src={`${process.env.PUBLIC_URL}/img/Avatar.png`} alt="Avatar" />
-        <h3>User info:</h3>
-        <div className="user-data">Name Surname</div>
-        <div className="user-data">Username</div>
-        <div className="user-data">User email</div>
-        <div className="user-data">User phone number</div>
-        <h4>User address:</h4>
-        <div className="user-data">Street</div>
-        <div className="user-data">Suiter</div>
-        <div className="user-data">City</div>
-        <div className="user-data">Zip code</div>
-        <h5>User Company:</h5>
-        <div className="user-data">Web site</div>
-        <div className="user-data">Company name</div>
-        <div className="user-data">Catch phrase</div>
-        <div className="user-data">Bs</div>
-        <button>Edit profile</button>
+      <div className="dashboard-left-menu-1">
+        <div className="dashboard-left-menu-img">
+          <img src={`${process.env.PUBLIC_URL}/img/Avatar.png`} alt="Avatar" />
+        </div>
+        <div className="dashboard-left-menu-user-info">
+          <h3>User info:</h3>
+          <div className="user-data">Name Surname</div>
+          <div className="user-data">Username</div>
+          <div className="user-data">User email</div>
+          <div className="user-data">User phone number</div>
+        </div>
+      </div>
+      <div className="dashboard-left-menu-2">
+        <div className="dashboard-left-menu-user-address">
+          <h3>User address:</h3>
+          <div className="user-data">Street</div>
+          <div className="user-data">Suiter</div>
+          <div className="user-data">City</div>
+          <div className="user-data">Zip code</div>
+        </div>
+        <div className="dashboard-left-menu-user-company">
+          <h3>User Company:</h3>
+          <div className="user-data">Web site</div>
+          <div className="user-data">Company name</div>
+          <div className="user-data">Catch phrase</div>
+          <div className="user-data">Bs</div>
+        </div>
+      </div>
+        <div className="dashboard-left-menu-button">
+          <button>Edit profile</button>
+        </div>
       </div>
     </div>
     </div>
