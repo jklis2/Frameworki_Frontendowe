6b6b34b306be2a0b72fb10a48eed39f2dcd58a6a
Matd127 <79983536+Matd127@users.noreply.github.com>
2022-11-09 14:48:16 +0100

    update css in PhotoList

--- ShareItApp/src/Component/tsx/UserList.tsx
+++ ShareItApp/src/Component/tsx/UserList.tsx
@@ -8,19 +8,28 @@ const UserList = (props: any) => {
     return obj.name.toLowerCase().includes(props.selectedUser.toString().toLowerCase());
   });
 
+  console.log(userFilter)
+
   return (
-    <div className="d-flex flex-wrap">
+    <div className="userlist__container">
+      <div className="userlist__items">
       {userFilter.map((user: User) => {
         return (
-          <Card key={user.id} border="info" className="list text-center">
+          <Card key={user.id} border="info" className="userlist text-center">
             <Card.Header>{user.username}</Card.Header>
             <Card.Body>
               <Card.Title>{user.name}</Card.Title>
-              <Card.Text>Info about user</Card.Text>
+              <Card.Text>
+                <b>E-mail:</b> {user.email}<br/>
+                <b>Phone number:</b> {user.phone}<br/>
+                <b>Website: </b>{user.website}
+              </Card.Text>
             </Card.Body>
           </Card>
         );
       })}
+      </div>
+      
     </div>
   );
 };
--- ShareItApp/src/Entities/Users.tsx
+++ ShareItApp/src/Entities/Users.tsx
@@ -1,8 +1,13 @@
+import Address from './Address'
+
 interface User {
     id: number,
     name: string,
     username: string, 
     email: string, 
+    phone: string,
+    website: string,
+    address: []
 }
 
 export default User
