b8e636843061cbc5c3609531dd697e4364cce094
Jakub Kliś <jakubklis2201@gmail.com>
2022-11-09 13:30:17 +0100

    PhotoList format

--- ShareItApp/src/Component/tsx/PhotoList.tsx
+++ ShareItApp/src/Component/tsx/PhotoList.tsx
@@ -1,47 +1,41 @@
 import React, { FC } from "react";
 import Photo from "../../Entities/Photo";
-import '../css/PhotoList.css'
+import "../css/PhotoList.css";
 
-interface Props{
-    items: Photo[];
-    selectedPhoto : number;
+interface Props {
+  items: Photo[];
+  selectedPhoto: number;
 }
-const PhotoList : FC<Props> = (props: any) => {
+const PhotoList: FC<Props> = (props: any) => {
+  const photoFilter = props.items.filter((obj: Photo) => {
+    return obj.id === Number(props.selectedPhoto);
+  });
 
-        const photoFilter = props.items.filter((obj : Photo) => {
-          return obj.id === Number(props.selectedPhoto);
-        })
+  console.log(Number(props.selectedPhoto));
 
-        console.log(Number(props.selectedPhoto))
-      
-    return(
-        <div className="photos__container">
-           <div className="photos__items">
-
-            {isNaN(Number(props.selectedPhoto)) || props.selectedPhoto === '' || props.selectedPhoto === 0 ?
-
-props.items.slice(0, 24).map((photo: Photo) => {
-    return(
-        <div className="photos__img" key={photo.id}>
-            <img src={photo.thumbnailUrl} alt="123"></img>
-        </div>
-    )
-
-})
-:
-photoFilter.slice(0, 24).map((photo: Photo) => {
-    return(
-        <div className="photos__img" key={photo.id}>
-            <img src={photo.thumbnailUrl} alt="123"></img>
-        </div>
-    )
-
-})
-}
-            
-        </div>
+  return (
+    <div className="photos__container">
+      <div className="photos__items">
+        {isNaN(Number(props.selectedPhoto)) ||
+        props.selectedPhoto === "" ||
+        props.selectedPhoto === 0
+          ? props.items.slice(0, 24).map((photo: Photo) => {
+              return (
+                <div className="photos__img" key={photo.id}>
+                  <img src={photo.thumbnailUrl} alt="123"></img>
+                </div>
+              );
+            })
+          : photoFilter.slice(0, 24).map((photo: Photo) => {
+              return (
+                <div className="photos__img" key={photo.id}>
+                  <img src={photo.thumbnailUrl} alt="123"></img>
+                </div>
+              );
+            })}
+      </div>
     </div>
-    )
-}
+  );
+};
 
-export default PhotoList
+export default PhotoList;
