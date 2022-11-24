68b7f58b24a7f813a5a26bece0bea8d2c78bfde8
matd127 <mateusz333555@gmail.com>
2022-11-24 19:05:25 +0100

    Changed logged user in Dashboard to current user

--- ShareItApp/src/Routers/Dashboard.tsx
+++ ShareItApp/src/Routers/Dashboard.tsx
@@ -9,10 +9,10 @@ import { useDispatch, useSelector } from "react-redux";
 import { Post } from "../Entities/Posts";
 import { Album } from "../Entities/Albums";
 import Photo from "../Entities/Photo";
-import Box from '@mui/material/Box';
-import Tab from '@mui/material/Tab';
-import TabContext from '@mui/lab/TabContext';
-import TabList from '@mui/lab/TabList';
+import Box from "@mui/material/Box";
+import Tab from "@mui/material/Tab";
+import TabContext from "@mui/lab/TabContext";
+import TabList from "@mui/lab/TabList";
 
 type GetUsers = ReturnType<typeof getUsers>;
 const Dashboard: FC = () => {
@@ -22,15 +22,17 @@ const Dashboard: FC = () => {
     dispatch<GetUsers>(getUsers());
   }, [dispatch]);
 
-  const { users, currentUser } = useSelector<IState, IUsersReducer>((state) => ({
-    ...state.users,
-  }));
-  
-  console.log(currentUser)
-  const[posts, setPosts] = useState<Array<Post>>([])
-  const[albums, setAlbums] = useState<Array<Album>>([])
-  const[photo, setPhoto] = useState<Array<Photo>>([]);
-  const [value, setValue] = React.useState('1');
+  const {currentUser } = useSelector<IState, IUsersReducer>(
+    (state) => ({
+      ...state.users,
+    })
+  );
+
+  console.log(currentUser);
+  const [posts, setPosts] = useState<Array<Post>>([]);
+  const [albums, setAlbums] = useState<Array<Album>>([]);
+  const [photo, setPhoto] = useState<Array<Photo>>([]);
+  const [value, setValue] = React.useState("1");
 
   const handleChange = (event: React.SyntheticEvent, newValue: string) => {
     setValue(newValue);
@@ -41,7 +43,7 @@ const Dashboard: FC = () => {
     const response = await request.json();
     setPosts(response);
   };
-  
+
   const fetchAlbums = async () => {
     const request = await fetch("https://jsonplaceholder.typicode.com/albums");
     const response = await request.json();
@@ -58,14 +60,14 @@ const Dashboard: FC = () => {
     fetchPosts();
     fetchAlbums();
     fetchPhotos();
-  },[])
+  }, []);
 
   const ownPosts = posts.filter((obj: Post) => {
-    return obj.userId === Number(users[0].id ?? 1)
-  })
+    return obj.userId === Number(currentUser?.id ?? 1);
+  });
   const ownAlbums = albums.filter((obj: Album) => {
-    return obj.userId === Number(users[0].id ?? 1)
-  })
+    return obj.userId === Number(currentUser?.id ?? 1);
+  });
 
   return (
     <>
@@ -73,92 +75,95 @@ const Dashboard: FC = () => {
         <Header />
         <div className="container">
           <div className="dashboard-left-menu ">
-            {users?.map((user) => {
+            <div className="!!!   TOMEK GIVE ME NAME   !!!">
+              <div className="dashboard-left-menu-1">
+                <div className="dashboard-left-menu-img">
+                  <img
+                    src={`${process.env.PUBLIC_URL}/img/Avatar.png`}
+                    alt="Avatar"
+                  />
+                </div>
+                <div className="dashboard-left-menu-user-info">
+                  <h3>User info:</h3>
+                  <div className="user-data">{currentUser?.name}</div>
+                  <div className="user-data">{currentUser?.username}</div>
+                  <div className="user-data">{currentUser?.email}</div>
+                  <div className="user-data">{currentUser?.phone}</div>
+                </div>
+              </div>
+              <div className="dashboard-left-menu-2">
+                <div className="dashboard-left-menu-user-address">
+                  <h3>User address:</h3>
+                  <div className="user-data">{currentUser?.address.street}</div>
+                  <div className="user-data">{currentUser?.address.suite}</div>
+                  <div className="user-data">{currentUser?.address.city}</div>
+                  <div className="user-data">
+                    {currentUser?.address.zipcode}
+                  </div>
+                </div>
+                <div className="dashboard-left-menu-user-company">
+                  <h3>User Company:</h3>
+                  <div className="user-data">
+                    {currentUser?.company.website}
+                  </div>
+                  <div className="user-data">{currentUser?.company.name}</div>
+                  <div className="user-data">
+                    {currentUser?.company.catchPhrase}
+                  </div>
+                  <div className="user-data">{currentUser?.company.bs}</div>
+                </div>
+              </div>
+              <div className="dashboard-left-menu-button">
+                <a className="dashboard-left-menu-button-a" href="/EditProfile">
+                  <button>Edit profile</button>
+                </a>
+              </div>
+            </div>
+          </div>
+
+          <div className="posts">
+            {ownPosts?.map((post) => {
               return (
-                <div className="!!!   TOMEK GIVE ME NAME   !!!">
-                  <div className="dashboard-left-menu-1">
-                    <div className="dashboard-left-menu-img">
-                      <img
-                        src={`${process.env.PUBLIC_URL}/img/Avatar.png`}
-                        alt="Avatar"
-                      />
-                    </div>
-                    <div className="dashboard-left-menu-user-info">
-                      <h3>User info:</h3>
-                      <div className="user-data">{user.name}</div>
-                      <div className="user-data">{user.username}</div>
-                      <div className="user-data">{user.email}</div>
-                      <div className="user-data">{user.phone}</div>
-                    </div>
-                  </div>
-                  <div className="dashboard-left-menu-2">
-                    <div className="dashboard-left-menu-user-address">
-                      <h3>User address:</h3>
-                      <div className="user-data">{user.address.street}</div>
-                      <div className="user-data">{user.address.suite}</div>
-                      <div className="user-data">{user.address.city}</div>
-                      <div className="user-data">{user.address.zipcode}</div>
-                    </div>
-                    <div className="dashboard-left-menu-user-company">
-                      <h3>User Company:</h3>
-                      <div className="user-data">{user.company.website}</div>
-                      <div className="user-data">{user.company.name}</div>
-                      <div className="user-data">
-                        {user.company.catchPhrase}
-                      </div>
-                      <div className="user-data">{user.company.bs}</div>
-                    </div>
-                  </div>
-                  <div className="dashboard-left-menu-button">
-                    <a
-                      className="dashboard-left-menu-button-a"
-                      href="/EditProfile"
-                    >
-                      <button>Edit profile</button>
-                    </a>
-                  </div>
+                <div className="single-post">
+                  <h2>{post.title}</h2>
+                  <p>{post.body}</p>
                 </div>
               );
             })}
-          </div> 
-            
-          <div className="posts"> 
-
-          {ownPosts?.map((post) => {
-              return (
-               <div className="single-post">
-                <h2>{post.title}</h2>
-                <p>{post.body}</p>
-               </div>
-              )
-          })}
           </div>
           <h2> Albums with photos:</h2>
-          {/* Delete in the future */}
           <div className="test-tabs">
-          <Box sx={{ width: '100%', typography: 'body1'}}>
-        <TabContext value={value}>
-        <Box sx={{ borderBottom: 1, borderColor: 'divider' }}>
-          <TabList variant="scrollable" onChange={handleChange} aria-label="Photo gallery">
-            {ownAlbums?.map((album) => {
-              return (
-                <Tab label={album.title} value={album.id} />
-              )
-            })
-          }
-          </TabList>
-        </Box>
-        <div className="photos d-flex justify-content-center flex-wrap">
-        {photo?.filter((photo) => {
-          return photo.albumId === Number(value)
-        }).map((ph: Photo) => {
-          return(
-          <div className="photoItem"><img src={ph.thumbnailUrl} alt={(ph.id).toString()}></img></div>
-        )})}
-        </div>
-      </TabContext>
-      
-    </Box>
+            <Box sx={{ width: "100%", typography: "body1" }}>
+              <TabContext value={value}>
+                <Box sx={{ borderBottom: 1, borderColor: "divider" }}>
+                  <TabList
+                    variant="scrollable"
+                    onChange={handleChange}
+                    aria-label="Photo gallery"
+                  >
+                    {ownAlbums?.map((album) => {
+                      return <Tab label={album.title} value={album.id} />;
+                    })}
+                  </TabList>
+                </Box>
+                <div className="photos d-flex justify-content-center flex-wrap">
+                  {photo
+                    ?.filter((photo) => {
+                      return photo.albumId === Number(value);
+                    })
+                    .map((ph: Photo) => {
+                      return (
+                        <div className="photoItem">
+                          <img
+                            src={ph.thumbnailUrl}
+                            alt={ph.id.toString()}
+                          ></img>
+                        </div>
+                      );
+                    })}
+                </div>
+              </TabContext>
+            </Box>
           </div>
         </div>
       </div>
