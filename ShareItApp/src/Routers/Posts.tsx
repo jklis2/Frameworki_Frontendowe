ec10b22d6517af53391f60a84bb61b4a7f2e98f0
Matd127 <79983536+Matd127@users.noreply.github.com>
2022-11-29 14:00:14 +0100

    Info about post author, and comment author

--- ShareItApp/src/Routers/Posts.tsx
+++ ShareItApp/src/Routers/Posts.tsx
@@ -3,12 +3,29 @@ import Header from "../Component/tsx/Header";
 import { Post } from "../Entities/Posts";
 import { useEffect } from "react";
 import { Comment } from "../Entities/Comments";
+import Accordion from "react-bootstrap/Accordion";
+import { useDispatch, useSelector } from "react-redux";
+import { IState } from "../Redux/Reducers";
+import { IUsersReducer } from "../Redux/Reducers/usersReducer";
+import { getUsers } from "../Redux/actions/userActions";
+import { User } from "../Entities/Users";
 
+type GetUsers = ReturnType<typeof getUsers>;
 const Posts: FC = () => {
-  
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch<GetUsers>(getUsers());
+  }, [dispatch]);
+
+  const { users, currentUser } = useSelector<IState, IUsersReducer>(
+    (state) => ({
+      ...state.users,
+    })
+  );
+
   const [posts, setPosts] = useState<Array<Post>>([]);
   const [comments, setComments] = useState<Array<Comment>>([]);
-
 
   const fetchPosts = async () => {
     const request = await fetch("https://jsonplaceholder.typicode.com/posts");
@@ -17,7 +34,9 @@ const Posts: FC = () => {
   };
 
   const fetchComments = async () => {
-    const request = await fetch("https://jsonplaceholder.typicode.com/comments");
+    const request = await fetch(
+      "https://jsonplaceholder.typicode.com/comments"
+    );
     const response = await request.json();
     setComments(response);
   };
@@ -30,24 +49,43 @@ const Posts: FC = () => {
   return (
     <div className="d-flex flex-column">
       <Header></Header>
-      <div>
+      <div className="d-flex flex-column align-items-center">
         {posts.map((post) => {
           return (
-            <div>
-              <h2>{post.title}</h2>
+            <div className="w-75">
+              <h2>
+                {" "}
+                {post.title} ~{" "}
+                {users
+                  .filter((user) => {
+                    return user.id === post.userId;
+                  })
+                  .map((user: User) => {
+                    return <span>{user.name}</span>;
+                  })}
+              </h2>
               <p>{post.body}</p>
 
-              <>{console.log(comments)}</>
-              <h3>Comments:</h3>
-              <br/>
-              {comments.filter((comment) => {
-                return comment.postId === post.id
-              }).map((cm: Comment) => {
-                return(<div>
-                  <h4>{cm.name}</h4>
-                  <p>{cm.body}</p>
-                </div>)
-              })}
+              <Accordion className="bg-transparent">
+                <Accordion.Item eventKey="0" className="bg-transparent">
+                  <Accordion.Header>Comments</Accordion.Header>
+                  <br />
+                  {comments
+                    .filter((comment) => {
+                      return comment.postId === post.id;
+                    })
+                    .map((cm: Comment) => {
+                      return (
+                        <Accordion.Body>
+                          <h3>
+                            {cm.name} ~ {cm.email}
+                          </h3>
+                          {cm.body}
+                        </Accordion.Body>
+                      );
+                    })}
+                </Accordion.Item>
+              </Accordion>
             </div>
           );
         })}
