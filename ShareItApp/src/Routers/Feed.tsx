b1ebc82d6c3f6dfa0f23048adbd1631d68dc7d81
Matd127 <79983536+Matd127@users.noreply.github.com>
2022-12-06 13:17:54 +0100

    Add photos to feed

--- ShareItApp/src/Routers/Feed.tsx
+++ ShareItApp/src/Routers/Feed.tsx
@@ -1,9 +1,52 @@
-import React, { FC } from "react";
 import Header from "../Component/tsx/Header";
+import PhotoList from "../Component/tsx/PhotoList";
+import React, { FC, useEffect, useState } from "react";
+import Photo from "../Entities/Photo";
+import PaginationMenu from "../Component/tsx/PaginationMenu";
 
 const Feed: FC = () => {
+
+  const [data, setData] = useState<Array<Photo>>([]);
+  const [photo, setPhoto] = useState(0);
+
+  const fetchData = async () => {
+    const request = await fetch("https://jsonplaceholder.typicode.com/photos");
+    const response = await request.json();
+    setData(response);
+  };
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+
+  //Change it for function later pls
+  const [page, setPage] = useState(1);
+  let noOfItems;
+  let noOfPages = 1;
+  let indexOfLastPhoto;
+  let indexOfFirstPhoto;
+  if (photo !== 0 && photo > 0) {
+    noOfItems = photo;
+    indexOfFirstPhoto = 0;
+    indexOfLastPhoto = data.length;
+  } else {
+    noOfItems = data.length;
+    noOfPages = Math.ceil(noOfItems / 24);
+    indexOfLastPhoto = page * 24;
+    indexOfFirstPhoto = indexOfLastPhoto - 24;
+  }
+  const currentPhostos = data.slice(indexOfFirstPhoto, indexOfLastPhoto);
+
+
   return (
-  <div> <Header></Header> Feed </div>);
+  <div> 
+    <Header></Header> 
+    <PhotoList items={currentPhostos} selectedPhoto={photo}/>
+    <PaginationMenu page={page} pages={noOfPages} setPage={setPage} />
+
+  
+  </div>);
 };
 
 export default Feed;
